3481a3d34ebd21729d00d85b9d3d30b030f8facd
Kamil Bachara <szpala27189@pwsztar.edu.pl>
2017-04-26 10:42:02 +0200

    exercise 2 and test report

--- test/app.spec.js
+++ test/app.spec.js
@@ -9,6 +9,12 @@ describe('reverseNumber', function ()
     {
         expect(app.reverseNumber(123)).to.eql(321);
     });
+
+    it('if is not a number', function()
+    {
+        expect(app.reverseNumber('asd2')).to.eql(false);
+    });
+
 });
 
 describe('returnOnlyLetter', function ()
@@ -18,12 +24,27 @@ describe('returnOnlyLetter', function ()
         expect(app.returnOnlyLetter('a1b4c.')).to.eql('abc');
     });
 
+    it('should return only letters', function()
+    {
+        expect(app.returnOnlyLetter(12323)).to.eql(false);
+    });
+
+    it('should return only letters', function()
+    {
+        expect(app.returnOnlyLetter('aaaa')).to.eql('aaaa');
+    });
+
 });
 
 describe('isEmail', function ()
 {
-    it('should return true when is email', function()
+    it('should return true when it is a email', function()
     {
         expect(app.isEmail('aaa@bbb.pl')).to.eql(true);
+    });
+
+    it('should return false when it is not a email', function()
+    {
+        expect(app.isEmail('aaabb.pl')).to.eql(false);
     });
 });
