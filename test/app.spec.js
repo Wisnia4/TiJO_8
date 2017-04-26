dded370b67043b443c1960790554e37b30747ae3
Kamil Bachara <szpala27189@pwsztar.edu.pl>
2017-04-26 10:25:45 +0200

    add report text from terminal

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,25 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('should revert 123 to 321', function()
+    {
+        expect(app.reverseNumber(123)).to.eql(321);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
+    it('should return only letters', function()
+    {
+        expect(app.returnOnlyLetter('a1b4c.')).to.eql('abc');
+    });
 
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true when is email', function()
+    {
+        expect(app.isEmail('aaa@bbb.pl')).to.eql(true);
+    });
 });
